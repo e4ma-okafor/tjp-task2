a0666dd7a3e6d22d8fa6f64fd4b0f7d5dd1228dc
Okafor Ifeoma <66106275+e4ma-okafor@users.noreply.github.com>
2023-07-13 08:59:42 +0100

    Updated website

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const hamburger = document.querySelector(".hamburger");
 const nav2 = document.querySelector(".nav2");
 const close = document.querySelector(".close-menu");
 const menu = document.querySelector(".menu-bar");
+const header = document.querySelector(".header");
 
 menu.addEventListener('click', () => {    
     hamburger.classList.toggle('active');
     close.classList.toggle('active');
-    nav2.classList.toggle('visible');             
+    nav2.classList.toggle('visible');  
+    header.classList.toggle('menu');                      
 })
